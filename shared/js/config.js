5ab0dc86b31e1571bba7bbf52fab71849888453a
Ogulcan Erdag <ogulcanerdag@gmail.com>
2025-12-28 13:52:48 +0100

    api base url changed for prod

--- shared/js/config.js
+++ shared/js/config.js
@@ -3,7 +3,9 @@ const GUEST_LOGIN = {
   password: "Guest1234!",
 };
 
-const API_BASE_URL = "http://127.0.0.1:8000/api/";
+// const API_BASE_URL = "http://127.0.0.1:8000/api/";
+
+const API_BASE_URL = "https://api.kanmind.ogulcan-erdag.com/api/";
 
 const LOGIN_URL = "login/";
 
